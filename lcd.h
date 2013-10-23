0ba86bd13b5324d72f315fbfc02c22163f1741ff
Brandon Belcher <c15brandon.belcher@usafa.edu>
2013-10-23 11:02:38 -0600

    First commit of the files. Original Code from what was given

---
+++ lcd.h
@@ -0,0 +1,27 @@
+/*
+ * lcd.h
+ *
+ *  Created on: Oct 22, 2013
+ *      Author: C15Brandon.Belcher
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+void initSPI();
+
+void LCDinit();
+
+void LCDclear();
+
+void cursorToLineTwo();
+
+void cursorToLineOne();
+
+void writeChar(char asciiChar);
+
+void writeString(char * string);
+
+void scrollString(char * string1, char * string2);
+
+#endif /* LCD_H_ */
